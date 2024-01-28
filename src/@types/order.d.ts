228db802f3906e753e59e6d1017bc552f5458ce8
Pedro Marins <ph.marins@hotmail.com>
2024-01-27 21:47:10 -0300

    feat: add basic structure

---
+++ src/@types/order.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+namespace Order {
+  interface Data {
+    id: number;
+    order_id: number;
+    status: Steps;
+    user_id: number;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+
+  type Steps = 'IN_PREPARATION' | 'READY' | 'FINISHED';
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { OrderController } from 'src/adapters/controllers/order.controller';
 import { PrismaHelper } from 'src/adapters/database/helpers/prisma.helper';
+import { OrderRepository } from 'src/adapters/database/repositories/order.repository';
+import { OrderService } from 'src/application/services/order.service';
+import { GetQueueOrderUseCase } from 'src/application/usecases/get-queue.usecase';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [PrismaHelper],
+  controllers: [OrderController],
+  providers: [
+    OrderService,
+    GetQueueOrderUseCase,
+    {
+      provide: 'OrderRepository',
+      useClass: OrderRepository,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+    PrismaHelper,
+  ],
 })
 export class AppModule {}
