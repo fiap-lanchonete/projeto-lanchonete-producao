689f2a00e15bae1407e45fad13fdf19582a42188
Pedro Marins <ph.marins@hotmail.com>
2024-01-27 23:07:29 -0300

    feat: add tests

---
+++ src/application/usecases/get-queue.usecase.spec.ts
@@ -0,0 +1,38 @@
+import { OrderRepository } from 'src/adapters/database/repositories/order.repository';
+import { OrderService } from 'src/application/services/order.service';
+import { GetQueueOrderUseCase } from 'src/application/usecases/get-queue.usecase';
+
+describe('GetOrderQueueUseCase', () => {
+  let getOrderQueueUseCase: GetQueueOrderUseCase;
+  let mockOrderService: OrderService;
+
+  beforeEach(() => {
+    const mockOrderRepository = {} as OrderRepository;
+    mockOrderService = new OrderService(mockOrderRepository);
+
+    getOrderQueueUseCase = new GetQueueOrderUseCase(mockOrderService);
+  });
+
+  describe('execute', () => {
+    it('should return a queue of orders', async () => {
+      const mockedQueue: Order.Data[] = [
+        {
+          id: 1,
+          order_id: 2,
+          status: 'IN_PREPARATION',
+          user_id: 23,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+
+      jest
+        .spyOn(mockOrderService, 'findByStatus')
+        .mockResolvedValue(mockedQueue);
+
+      const result = await getOrderQueueUseCase.execute();
+
+      expect(result).toEqual(mockedQueue);
+    });
+  });
+});
